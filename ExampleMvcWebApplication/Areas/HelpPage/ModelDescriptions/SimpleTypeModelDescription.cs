4d07b67d533b569f19d23738a206cc261107da95
Kallyn Gowdy <kallyngowdy@gmail.com>
2014-08-15 20:51:05 -0400

    Added Example Web App

---
+++ ExampleMvcWebApplication/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExampleMvcWebApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ExampleMvcWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExampleMvcWebApplication.WebApiApplication" Language="C#" %>
